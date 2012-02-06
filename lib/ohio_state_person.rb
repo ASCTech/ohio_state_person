8ef77b85672aa59e14d6cbf7ab66a23021619b29
John Colvin <colvin.68@osu.edu>
2012-02-06 15:52:57 -0500

    Made name_n code conditional

--- lib/ohio_state_person.rb
+++ lib/ohio_state_person.rb
@@ -7,8 +7,10 @@ module OhioStatePerson
       extend ClassMethods
       include InstanceMethods
 
-      validates_uniqueness_of :name_n
-      validates_format_of :name_n, :with => /\A[a-z]([a-z-]*[a-z])?\.[1-9]\d*\z/, :message => 'must be in format: name.#'
+      if column_names.include? 'name_n'
+        validates_uniqueness_of :name_n
+        validates_format_of :name_n, :with => /\A[a-z]([a-z-]*[a-z])?\.[1-9]\d*\z/, :message => 'must be in format: name.#'
+      end
 
       validates_uniqueness_of :emplid
       validates_format_of :emplid, :with => /\A\d{8,9}\z/, :message => 'must be 8 or 9 digits'
@@ -23,7 +25,7 @@ module OhioStatePerson
       q = q.to_s.strip
       h = ActiveSupport::OrderedHash.new
       h[/\A\d+\z/]        = lambda { where(:emplid => q) }
-      h[/\A\D+\.\d+\z/]   = lambda { where(:name_n => q) }
+      h[/\A\D+\.\d+\z/]   = lambda { where(:name_n => q) } if column_names.include? 'name_n'
       h[/(\S+),\s*(\S+)/] = lambda { where('last_name LIKE ? AND first_name LIKE ?', $1, "#{$2}%") }
       h[/(\S+)\s+(\S+)/]  = lambda { where('first_name LIKE ? AND last_name LIKE ?', $1, "#{$2}%") }
       h[/\S/]             = lambda { where('last_name LIKE ?', "#{q}%") }
@@ -38,6 +40,8 @@ module OhioStatePerson
 
   module InstanceMethods
     def email
+      return self[:email] if self.class.column_names.include? 'email'
+      return nil      unless self.class.column_names.include? 'name_n'
       name_n.present? ? "#{name_n}@osu.edu" : ''
     end
 
